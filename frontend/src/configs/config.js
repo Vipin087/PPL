4c167707f4c7e2330e29a93e323ea64c9678007f
Vipin Sharma <vipin.sharma@daffodilsw.com>
2021-05-27 12:40:47 +0530

    wae

---
+++ frontend/src/configs/config.js
@@ -0,0 +1,18 @@
+const url="http://localhost:3001/"
+
+var URL={
+  checkUserExist:url+"checkUserExist",
+  sendData:url+"sendData",
+  imageData:url+"imageData",
+  sendImageId:url+"sendImageId",
+  totalLike:url+"TotalLike",
+  url:url,
+  getImage:url+"getImage",
+  commentData:url+"CommentData", 
+  checkEmail:url+"CheckEmail", 
+  updatePassword:url+"UpdatePassword",
+  profile:url+"Profile"
+
+}
+
+module.exports=URL
